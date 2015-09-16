40ca8847eee3b69d345632ea74ade62ebc3a8a54
luscus <luscus.redbeard@gmail.com>
2015-09-16 14:33:24 +0200

    added support for nested arrays

--- test/unit/deep-lib.spec.js
+++ test/unit/deep-lib.spec.js
@@ -18,6 +18,25 @@ var data = {
           "area": 755
         }
       }
+    },
+    "spain": {
+      "sites": [
+          "Alhambra",
+          "Cabo de Gata"
+      ],
+      "capital": "Madrid",
+      "towns": [
+        {
+          "name": "Madrid",
+          "population": 3165235,
+          "area": 605.770
+        },
+        {
+          "name": "Barcelona",
+          "population": 1751775,
+          "area": 755
+        }
+      ]
     }
   }
 };
@@ -29,7 +48,7 @@ describe('[' + require('path').basename(__filename) + '] - clone ', function() {
     expect(clone).to.not.equal(data);
   });
 
-  it('should not have the same structure', function() {
+  it('should have the same structure', function() {
     expect(clone).to.deep.equal(data);
   });
 
@@ -40,7 +59,7 @@ describe('[' + require('path').basename(__filename) + '] - clone ', function() {
     expect(data.countries.germany.towns.capital).to.equal('berlin');
   });
 
-  it('should clone substructure', function() {
+  it('should work with substructures', function() {
     var clone = deep.clone(data.countries.germany);
     expect(clone).to.deep.equal(data.countries.germany);
   });
@@ -52,6 +71,16 @@ describe('[' + require('path').basename(__filename) + '] - get ', function() {
   it('should return a deep value', function() {
     var capital = deep.get(clone, 'countries.germany.towns.capital');
     expect(capital).to.equal('berlin');
+  });
+
+  it('should return a deep array element', function() {
+    var capital = deep.get(clone, 'countries.spain.sites.0');
+    expect(capital).to.equal('Alhambra');
+  });
+
+  it('should return a deep value in an array element', function() {
+    var capital = deep.get(clone, 'countries.spain.towns.0.name');
+    expect(capital).to.equal('Madrid');
   });
 
   it('should return deep structures', function() {
@@ -100,11 +129,59 @@ describe('[' + require('path').basename(__filename) + '] - add ', function() {
     expect(control).to.equal('paris');
   });
 
+  it('should add a root value', function() {
+    deep.add(clone, 'continents', ['europa']);
+
+    expect(clone.continents).to.deep.equal(['europa']);
+  });
+
+  it('should add a deep value in an array', function() {
+    deep.add(clone, 'countries.spain.sites.2', 'Gibraltar');
+    var control = deep.get(clone, 'countries.spain.sites.2');
+
+    expect(control).to.equal('Gibraltar');
+  });
+
+  it('should add a deep value in an array element', function() {
+    deep.add(clone, 'countries.spain.towns.0.website', 'http://www.madrid.es');
+    var control = deep.get(clone, 'countries.spain.towns.0.website');
+
+    expect(control).to.equal('http://www.madrid.es');
+  });
+
+  it('should create an array element to add a value', function() {
+    deep.add(clone, 'countries.spain.sites.2.website', 'http://www.madrid.es');
+    var control = deep.get(clone, 'countries.spain.sites.2.website');
+
+    expect(control).to.equal('http://www.madrid.es');
+  });
+
+  it('should create an array to add an element', function() {
+    deep.add(clone, 'countries.spain.array.0', 'sometest');
+    var control = deep.get(clone, 'countries.spain.array.0');
+
+    expect(control).to.equal('sometest');
+  });
+
+  it('should create an array to add an object element property value', function() {
+    deep.add(clone, 'countries.spain.receipts.0.name', 'Paella');
+    var control = deep.get(clone, 'countries.spain.receipts.0.name');
+
+    expect(control).to.equal('Paella');
+  });
+
   it('should add a deep structure', function() {
     deep.add(clone, 'countries.france.towns.paris', paris);
     var control = deep.get(clone, 'countries.france.towns.paris');
 
     expect(control).to.equal(paris);
+  });
+
+  it('should ignore undefined values and no substructure created', function() {
+    deep.add(clone, 'countries.portugal');
+    var control = deep.get(clone, 'countries.portugal.towns');
+
+    expect(control).to.equal(undefined);
   });
 });
 
@@ -117,6 +194,14 @@ describe('[' + require('path').basename(__filename) + '] - delete ', function() 
 
     expect(value).to.equal('berlin');
     expect(control).to.equal(undefined);
+  });
+
+  it('should delete an array element', function() {
+    var value   = deep.delete(clone, 'countries.spain.sites.0');
+    var control = deep.get(clone, 'countries.spain.sites.0');
+
+    expect(value).to.equal('Alhambra');
+    expect(control).to.equal('Cabo de Gata');
   });
 
   it('should return deep structures', function() {
@@ -169,9 +254,40 @@ describe('[' + require('path').basename(__filename) + '] - move ', function() {
     expect(control).to.equal(moved);
   });
 
+  it('should move an array element to a different array', function() {
+    var oldPath = 'countries.spain.sites.0';
+    var newPath = 'countries.france.sites.0';
+
+    var moved = deep.get(clone, oldPath);
+
+    deep.move(clone, oldPath, newPath);
+    var nextElement = deep.get(clone, oldPath);
+    var control     = deep.get(clone, newPath);
+    var original    = deep.get(data, oldPath);
+
+    expect(nextElement).to.equal('Cabo de Gata');
+    expect(control).to.deep.equal(original);
+    expect(control).to.equal(moved);
+  });
+
+  it('should move an array element to an object', function() {
+    var oldPath = 'countries.spain.sites.0';
+    var newPath = 'countries.spain.bestSite';
+
+    var moved = deep.get(clone, oldPath);
+
+    deep.move(clone, oldPath, newPath);
+    var empty   = deep.get(clone, oldPath);
+    var control = deep.get(clone, newPath);
+
+    expect(empty).to.equal(undefined);
+    expect(control).to.equal(moved);
+  });
+
   it('should move a deep value', function() {
     deep.add(clone, 'countries.france.towns.capital', 'hamburg');
     var control = deep.get(clone, 'countries.france.towns.capital');
+
 
     expect(control).to.equal('hamburg');
   });
@@ -183,6 +299,7 @@ describe('[' + require('path').basename(__filename) + '] - equal ', function() {
   it('should find data and clone equal', function() {
 
     var areEqual = deep.equal(clone, data);
+    expect(clone).to.deep.equal(data);
     expect(areEqual).to.equal(true);
   });
 
@@ -278,9 +395,18 @@ describe('[' + require('path').basename(__filename) + '] - getPaths ', function(
       'countries.germany.towns.berlin.name',
       'countries.germany.towns.berlin.population',
       'countries.germany.towns.berlin.area',
-      'countries.germany.towns.berlin.hamburg.name',
-      'countries.germany.towns.berlin.hamburg.population',
-      'countries.germany.towns.berlin.hamburg.area'
+      'countries.germany.towns.hamburg.name',
+      'countries.germany.towns.hamburg.population',
+      'countries.germany.towns.hamburg.area',
+      'countries.spain.sites.0',
+      'countries.spain.sites.1',
+      'countries.spain.capital',
+      'countries.spain.towns.0.name',
+      'countries.spain.towns.0.population',
+      'countries.spain.towns.0.area',
+      'countries.spain.towns.1.name',
+      'countries.spain.towns.1.population',
+      'countries.spain.towns.1.area'
     ]);
   });
 });
