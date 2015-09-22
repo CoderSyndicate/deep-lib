4f1a80f70e781012a5958955de0ce655087b7ca8
luscus <luscus.redbeard@gmail.com>
2015-09-22 21:50:54 +0200

    fixed indent

--- test/unit/methods/createPath.spec.js
+++ test/unit/methods/createPath.spec.js
@@ -18,107 +18,107 @@ var errorCodes = require('../../../lib/methods/createPath').errorCodes;
 
 describe('[' + __filename.substring(__filename.indexOf('/test/') + 1) + '] - createPath ', function() {
 
-    it('should create root object: object1', function() {
-        var clone = deep.clone(data);
+  it('should create root object: object1', function() {
+    var clone = deep.clone(data);
 
-        createPath(clone, 'object1');
+    createPath(clone, 'object1');
 
-        var diff = diffLib(data, clone);
+    var diff = diffLib(data, clone);
 
-        expect(clone).to.not.deep.equal(data);
-        expect(diff).to.deep.equal(
-          [{"kind": "N","path": ["object1"],"rhs": {}}]
-        );
-    });
+    expect(clone).to.not.deep.equal(data);
+    expect(diff).to.deep.equal(
+      [{"kind": "N","path": ["object1"],"rhs": {}}]
+    );
+  });
 
-    it('should throw error if first path element for an object is an array: 0', function() {
-        var clone = deep.clone(data);
+  it('should throw error if first path element for an object is an array: 0', function() {
+    var clone = deep.clone(data);
 
-        expect(createPath.bind(this,clone, '0')).to.throw(errorCodes.OBJECT_INVALID_FIRST_PATH_ELEMENT);
-    });
+    expect(createPath.bind(this,clone, '0')).to.throw(errorCodes.OBJECT_INVALID_FIRST_PATH_ELEMENT);
+  });
 
-    it('should throw error if first path element for an array is an array, and path element length is one: 0', function() {
-        var clone = deep.clone(data);
+  it('should throw error if first path element for an array is an array, and path element length is one: 0', function() {
+    var clone = deep.clone(data);
 
-        expect(createPath.bind(this,clone.countries.spain.sites, '0'))
-            .to.throw(errorCodes.ARRAY_INVALID_FIRST_PATH_ELEMENT);
-    });
+    expect(createPath.bind(this,clone.countries.spain.sites, '0'))
+      .to.throw(errorCodes.ARRAY_INVALID_FIRST_PATH_ELEMENT);
+  });
 
-    it('should push new array element if first path element is an array placeholder: '+tools.arrayPlaceholder+'.prop1', function() {
-        var clone = deep.clone(data);
+  it('should push new array element if first path element is an array placeholder: '+tools.arrayPlaceholder+'.prop1', function() {
+    var clone = deep.clone(data);
 
-        createPath(clone.countries.spain.sites, tools.arrayPlaceholder + '.prop1');
+    createPath(clone.countries.spain.sites, tools.arrayPlaceholder + '.prop1');
 
-        var diff = diffLib(data, clone);
+    var diff = diffLib(data, clone);
 
-        expect(clone).to.not.deep.equal(data);
-        expect(diff).to.deep.equal(
-            [{"kind":"A","path":["countries","spain","sites"],"index":2,"item":{"kind":"N","rhs":{"prop1":{}}}}]
-        );
-    });
+    expect(clone).to.not.deep.equal(data);
+    expect(diff).to.deep.equal(
+      [{"kind":"A","path":["countries","spain","sites"],"index":2,"item":{"kind":"N","rhs":{"prop1":{}}}}]
+    );
+  });
 
-    it('should push new array element if first path element is an array push placeholder: '+tools.arrayPush+'.prop1', function() {
-        var clone = deep.clone(data);
+  it('should push new array element if first path element is an array push placeholder: '+tools.arrayPush+'.prop1', function() {
+    var clone = deep.clone(data);
 
-        createPath(clone.countries.spain.sites, tools.arrayPush + '.prop1');
+    createPath(clone.countries.spain.sites, tools.arrayPush + '.prop1');
 
-        var diff = diffLib(data, clone);
+    var diff = diffLib(data, clone);
 
-        expect(clone).to.not.deep.equal(data);
-        expect(diff).to.deep.equal(
-            [{"kind":"A","path":["countries","spain","sites"],"index":2,"item":{"kind":"N","rhs":{"prop1":{}}}}]
-        );
-    });
+    expect(clone).to.not.deep.equal(data);
+    expect(diff).to.deep.equal(
+      [{"kind":"A","path":["countries","spain","sites"],"index":2,"item":{"kind":"N","rhs":{"prop1":{}}}}]
+    );
+  });
 
-    it('should unshift new array element if first path element is an array unshift placeholder: '+tools.arrayUnshift+'.prop1', function() {
-        var clone = deep.clone(data);
+  it('should unshift new array element if first path element is an array unshift placeholder: '+tools.arrayUnshift+'.prop1', function() {
+    var clone = deep.clone(data);
 
-        createPath(clone.countries.spain.sites, tools.arrayUnshift + '.prop1');
+    createPath(clone.countries.spain.sites, tools.arrayUnshift + '.prop1');
 
-        var diff = diffLib(data, clone);
+    var diff = diffLib(data, clone);
 
-        expect(clone).to.not.deep.equal(data);
-        expect(diff).to.deep.equal(
-            [{"kind":"E","path":["countries","spain","sites",0],"lhs":"Alhambra","rhs":{"prop1":{}}},{"kind":"E","path":["countries","spain","sites",1],"lhs":"Cabo de Gata","rhs":"Alhambra"},{"kind":"A","path":["countries","spain","sites"],"index":2,"item":{"kind":"N","rhs":"Cabo de Gata"}}]
-        );
-    });
+    expect(clone).to.not.deep.equal(data);
+    expect(diff).to.deep.equal(
+      [{"kind":"E","path":["countries","spain","sites",0],"lhs":"Alhambra","rhs":{"prop1":{}}},{"kind":"E","path":["countries","spain","sites",1],"lhs":"Cabo de Gata","rhs":"Alhambra"},{"kind":"A","path":["countries","spain","sites"],"index":2,"item":{"kind":"N","rhs":"Cabo de Gata"}}]
+    );
+  });
 
-    it('should create deep object in a new array element: '+tools.arrayPlaceholder+'.lvl1.lvl2', function() {
-        var clone = deep.clone(data);
+  it('should create deep object in a new array element: '+tools.arrayPlaceholder+'.lvl1.lvl2', function() {
+    var clone = deep.clone(data);
 
-        createPath(clone.countries.spain.sites, tools.arrayPlaceholder + '.lvl1.lvl2');
+    createPath(clone.countries.spain.sites, tools.arrayPlaceholder + '.lvl1.lvl2');
 
-        var diff = diffLib(data, clone);
+    var diff = diffLib(data, clone);
 
-        expect(clone).to.not.deep.equal(data);
-        expect(diff).to.deep.equal(
-            [{"kind":"A","path":["countries","spain","sites"],"index":2,"item":{"kind":"N","rhs":{"lvl1":{"lvl2":{}}}}}]
-        );
-    });
+    expect(clone).to.not.deep.equal(data);
+    expect(diff).to.deep.equal(
+      [{"kind":"A","path":["countries","spain","sites"],"index":2,"item":{"kind":"N","rhs":{"lvl1":{"lvl2":{}}}}}]
+    );
+  });
 
-    it('should overwrite array element if first path element is an index: 0.lvl1', function() {
-        var clone = deep.clone(data);
+  it('should overwrite array element if first path element is an index: 0.lvl1', function() {
+    var clone = deep.clone(data);
 
-        createPath(clone.countries.spain.sites, '0.lvl1');
+    createPath(clone.countries.spain.sites, '0.lvl1');
 
-        var diff = diffLib(data, clone);
+    var diff = diffLib(data, clone);
 
-        expect(clone).to.not.deep.equal(data);
-        expect(diff).to.deep.equal(
-            [{"kind":"E","path":["countries","spain","sites",0],"lhs":"Alhambra","rhs":{"lvl1":{}}}]
-        );
-    });
+    expect(clone).to.not.deep.equal(data);
+    expect(diff).to.deep.equal(
+      [{"kind":"E","path":["countries","spain","sites",0],"lhs":"Alhambra","rhs":{"lvl1":{}}}]
+    );
+  });
 
-    it('should create deep object behind an array: 0.lvl1.lvl2 rebasing root to countries.spain.sites', function() {
-        var clone = deep.clone(data);
+  it('should create deep object behind an array: 0.lvl1.lvl2 rebasing root to countries.spain.sites', function() {
+    var clone = deep.clone(data);
 
-        createPath(clone, '0.lvl1.lvl2', 'countries.spain.sites');
+    createPath(clone, '0.lvl1.lvl2', 'countries.spain.sites');
 
-        var diff = diffLib(data, clone);
+    var diff = diffLib(data, clone);
 
-        expect(clone).to.not.deep.equal(data);
-        expect(diff).to.deep.equal(
-            [{"kind":"E","path":["countries","spain","sites",0],"lhs":"Alhambra","rhs":{"lvl1":{"lvl2":{}}}}]
-        );
-    });
+    expect(clone).to.not.deep.equal(data);
+    expect(diff).to.deep.equal(
+      [{"kind":"E","path":["countries","spain","sites",0],"lhs":"Alhambra","rhs":{"lvl1":{"lvl2":{}}}}]
+    );
+  });
 });
--- test/unit/tools.spec.js
+++ test/unit/tools.spec.js
@@ -1,5 +1,7 @@
 /* jshint node:true */
 /* jshint expr:true*/
+/* jshint -W024 */
+/* global __filename */
 /* global require */
 /* global process */
 /* global describe */
@@ -14,61 +16,61 @@ var testPath  = 'root' + tools.sep + 'depth1' + tools.sep + 'depth2' + tools.sep
 
 describe('[' + __filename.substring(__filename.indexOf('/test/') + 1) + '] - method ', function() {
 
-describe('split ', function() {
+  describe('split ', function() {
 
     it('should return an empty array if no path was provided', function() {
-        var result = tools.split();
+      var result = tools.split();
 
-        expect(result).to.deep.equal([]);
+      expect(result).to.deep.equal([]);
     });
 
     it('should an array with the path elements', function() {
-        var result = tools.split(testPath);
+      var result = tools.split(testPath);
 
-        expect(result.length).to.equal(4);
+      expect(result.length).to.equal(4);
     });
-});
+  });
 
-describe('join ', function() {
+  describe('join ', function() {
 
     it('should join path elements', function() {
-        var array  = tools.split(testPath);
-        var result = tools.join(array);
+      var array  = tools.split(testPath);
+      var result = tools.join(array);
 
-        expect(result).to.equal(testPath);
+      expect(result).to.equal(testPath);
     });
-});
+  });
 
-describe('dirname ', function() {
+  describe('dirname ', function() {
 
     it('should return the path argument if it has no depth', function() {
-        var result = tools.dirname('root');
+      var result = tools.dirname('root');
 
-        expect(result).to.equal('root');
+      expect(result).to.equal('root');
     });
 
     it('should return path string without last depth', function() {
-        var result    = tools.dirname(testPath);
-        var pathArray = tools.split(testPath).slice(0, 3);
+      var result    = tools.dirname(testPath);
+      var pathArray = tools.split(testPath).slice(0, 3);
 
-        expect(result).to.equal(tools.join(pathArray));
+      expect(result).to.equal(tools.join(pathArray));
     });
-});
+  });
 
-describe('basename ', function() {
+  describe('basename ', function() {
 
     it('should return the path argument if it has no depth', function() {
-        var result = tools.basename('root');
+      var result = tools.basename('root');
 
-        expect(result).to.equal('root');
+      expect(result).to.equal('root');
     });
 
     it('should return last depth', function() {
-        var result      = tools.basename(testPath);
-        var pathArray   = tools.split(testPath);
-        var lastElement = pathArray[pathArray.length - 1];
+      var result      = tools.basename(testPath);
+      var pathArray   = tools.split(testPath);
+      var lastElement = pathArray[pathArray.length - 1];
 
-        expect(result).to.equal(lastElement);
+      expect(result).to.equal(lastElement);
     });
-});
+  });
 });
