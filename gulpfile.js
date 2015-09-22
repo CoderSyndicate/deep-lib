933474baa4d283cb3db0ee468077ee6fa5b20774
luscus <luscus.redbeard@gmail.com>
2015-09-23 01:33:12 +0200

    second test

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,17 @@ var gulp = require('gulpfile.basics');
 var fs = require("fs");
 var gutil = require("gulp-util");
 var gulpJsdoc2md = require("gulp-jsdoc-to-markdown");
+var rename = require("gulp-rename");
 var concat = require("gulp-concat");
 
-gulp.task("docs", function() {
-  return gulp.src("lib/**/*.js")
-    .pipe(concat("all.md"))
+gulp.task("docs", function(){
+  return gulp.src("lib/*.js")
     .pipe(gulpJsdoc2md({}))
     .on("error", function(err){
-      gutil.log("jsdoc2md failed:", err.message);
+      gutil.log(gutil.colors.red("jsdoc2md failed"), err.message)
     })
+    .pipe(rename(function(path){
+      path.extname = ".md";
+    }))
     .pipe(gulp.dest("api"));
 });
