fce66619aa75330757b42f3416d5a04eb5df788e
luscus <luscus.redbeard@gmail.com>
2015-10-06 10:31:48 +0200

    changed update method and tests to be conform with defined behaviour

--- test/unit/methods/update.spec.js
+++ test/unit/methods/update.spec.js
@@ -14,7 +14,7 @@ var tools    = require('../../../lib/tools');
 var data     = require('../../object.json');
 
 var createPath = require('../../../lib/methods/createPath').createPath;
-var errorCodes = require('../../../lib/methods/createPath').errorCodes;
+var errorCodes = require('../../../lib/methods/update').errorCodes;
 
 describe('[' + __filename.substring(__filename.indexOf('/test/') + 1) + '] - createPath ', function() {
 
@@ -31,94 +31,70 @@ describe('[' + __filename.substring(__filename.indexOf('/test/') + 1) + '] - cre
         );
     });
 
-    it('should throw error if first path element for an object is an array: 0', function() {
+    it('should throw error if array wildcards are part of the path argument: bar.*', function() {
         var clone = deep.clone(data);
 
-        expect(createPath.bind(this,clone, '0')).to.throw(errorCodes.OBJECT_INVALID_FIRST_PATH_ELEMENT);
+        expect(deep.update.bind(this,clone, 'bar.*')).to.throw(errorCodes.WILDCARDS_NOT_ALLOWED);
     });
 
-    it('should throw error if first path element for an array is an array, and path element length is one: 0', function() {
+    it('should do nothing if value argument is undefined', function() {
         var clone = deep.clone(data);
 
-        expect(createPath.bind(this,clone.countries.spain.sites, '0'))
-            .to.throw(errorCodes.ARRAY_INVALID_FIRST_PATH_ELEMENT);
+        deep.update(clone, 'countries.spain.sites.0');
+
+        expect(clone).to.deep.equal(data);
     });
 
-    it('should push new array element if first path element is an array placeholder: '+tools.arrayPlaceholder+'.prop1', function() {
+    it('should throw error if provided path point to an unknown property: countries.ireland.sites.0', function() {
         var clone = deep.clone(data);
 
-        createPath(clone.countries.spain.sites, tools.arrayPlaceholder + '.prop1');
+        expect(deep.update.bind(this,clone, 'countries.ireland.sites.0', 'Giant\'s Causeway')).to.throw(errorCodes.UNKNOWN_PROPERTY);
+    });
+
+    it('should fail silently provided path points to an unknown property and ignore error argument is true: countries.ireland.sites.0', function() {
+        var clone = deep.clone(data);
+
+        deep.update(clone, 'countries.ireland.sites.0', 'Giant\'s Causeway', true);
+
+        expect(clone).to.deep.equal(data);
+    });
+
+    it('should overwrite array element: countries.spain.sites.0', function() {
+        var clone = deep.clone(data);
+
+        deep.update(clone, 'countries.spain.sites.0', 'Mojacar');
 
         var diff = diffLib(data, clone);
 
         expect(clone).to.not.deep.equal(data);
         expect(diff).to.deep.equal(
-            [{"kind":"A","path":["countries","spain","sites"],"index":2,"item":{"kind":"N","rhs":{"prop1":{}}}}]
+            [{"kind":"E","path":["countries","spain","sites",0],"lhs":"Alhambra","rhs":"Mojacar"}]
         );
     });
 
-    it('should push new array element if first path element is an array push placeholder: '+tools.arrayPush+'.prop1', function() {
+    it('should overwrite object property: countries.germany.capital', function() {
         var clone = deep.clone(data);
 
-        createPath(clone.countries.spain.sites, tools.arrayPush + '.prop1');
+        deep.update(clone, 'countries.germany.capital', 'Bonn');
 
         var diff = diffLib(data, clone);
 
         expect(clone).to.not.deep.equal(data);
         expect(diff).to.deep.equal(
-            [{"kind":"A","path":["countries","spain","sites"],"index":2,"item":{"kind":"N","rhs":{"prop1":{}}}}]
+            [{"kind":"N","path":["countries","germany","capital"],"rhs":"Bonn"}]
         );
     });
 
-    it('should unshift new array element if first path element is an array unshift placeholder: '+tools.arrayUnshift+'.prop1', function() {
+    it('should support root property overwrite: planet', function() {
         var clone = deep.clone(data);
 
-        createPath(clone.countries.spain.sites, tools.arrayUnshift + '.prop1');
+        deep.update(clone, 'planet', 'Mars');
 
         var diff = diffLib(data, clone);
 
         expect(clone).to.not.deep.equal(data);
         expect(diff).to.deep.equal(
-            [{"kind":"E","path":["countries","spain","sites",0],"lhs":"Alhambra","rhs":{"prop1":{}}},{"kind":"E","path":["countries","spain","sites",1],"lhs":"Cabo de Gata","rhs":"Alhambra"},{"kind":"A","path":["countries","spain","sites"],"index":2,"item":{"kind":"N","rhs":"Cabo de Gata"}}]
-        );
-    });
-
-    it('should create deep object in a new array element: '+tools.arrayPlaceholder+'.lvl1.lvl2', function() {
-        var clone = deep.clone(data);
-
-        createPath(clone.countries.spain.sites, tools.arrayPlaceholder + '.lvl1.lvl2');
-
-        var diff = diffLib(data, clone);
-
-        expect(clone).to.not.deep.equal(data);
-        expect(diff).to.deep.equal(
-            [{"kind":"A","path":["countries","spain","sites"],"index":2,"item":{"kind":"N","rhs":{"lvl1":{"lvl2":{}}}}}]
-        );
-    });
-
-    it('should overwrite array element if first path element is an index: 0.lvl1', function() {
-        var clone = deep.clone(data);
-
-        createPath(clone.countries.spain.sites, '0.lvl1');
-
-        var diff = diffLib(data, clone);
-
-        expect(clone).to.not.deep.equal(data);
-        expect(diff).to.deep.equal(
-            [{"kind":"E","path":["countries","spain","sites",0],"lhs":"Alhambra","rhs":{"lvl1":{}}}]
-        );
-    });
-
-    it('should create deep object behind an array: 0.lvl1.lvl2 rebasing root to countries.spain.sites', function() {
-        var clone = deep.clone(data);
-
-        createPath(clone, '0.lvl1.lvl2', 'countries.spain.sites');
-
-        var diff = diffLib(data, clone);
-
-        expect(clone).to.not.deep.equal(data);
-        expect(diff).to.deep.equal(
-            [{"kind":"E","path":["countries","spain","sites",0],"lhs":"Alhambra","rhs":{"lvl1":{"lvl2":{}}}}]
+            [{"kind":"E","path":["planet"],"lhs":"Earth","rhs":"Mars"}]
         );
     });
 });
